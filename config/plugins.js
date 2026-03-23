1fb20d0a490247be65b7acabe039ab390e89e166
hachalupa <bukovskisdeniss@gmail.com>
2026-03-23 21:52:53 +0200

    email configuration, password reset, user self-update

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,19 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: '@strapi/provider-email-nodemailer',
+      providerOptions: {
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+          user: env('GMAIL_USER'),
+          pass: env('GMAIL_PASS'),
+        },
+      },
+      settings: {
+        defaultFrom: env('GMAIL_USER'),
+        defaultReplyTo: env('GMAIL_USER'),
+      },
+    },
+  },
+})
